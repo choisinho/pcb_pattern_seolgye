05f1905803e5c29a557176efd174b91a4a9fbeb1
Sinho <choi_sinho@naver.com>
2022-05-31 11:46:10 +0900

    Update alarm.ino

--- raspberry_project/arduino_files/alarm/alarm.ino
+++ raspberry_project/arduino_files/alarm/alarm.ino
@@ -3,7 +3,7 @@
 const int bzpin = 15; //buzzer pin
 const int dpClk = 16; //display CLK
 const int dpDio = 17; //display DIO
-unsigned int cnt = 3600; //timer count
+unsigned int cnt = 10; //timer count
 
 TM1637Display display(dpClk, dpDio);
 
@@ -12,12 +12,11 @@ void setup()
   display.setBrightness(0x0a);
   while (true) {
     if (cnt == 0) {
-      display.showNumberDec(cnt)
-      tone(bzpin, 500, 3000)
-      display.show    
+      display.showNumberDec(cnt);
+      tone(bzpin, 500, 3000);    
       break;
     }
-    display.showNumberDec(cnt)
+    display.showNumberDec(cnt);
     cnt--;
     delay(1000);
   }
