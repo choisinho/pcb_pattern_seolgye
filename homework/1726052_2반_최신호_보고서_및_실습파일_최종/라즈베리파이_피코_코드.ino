7ee055c9931f349558194136b87aa5663ffbe70c
Sinho <choi_sinho@naver.com>
2022-06-19 12:23:26 +0900

    final exam

---
+++ homework/1726052_2반_최신호_보고서_및_실습파일_최종/라즈베리파이_피코_코드.ino
@@ -0,0 +1,28 @@
+#include <TM1637Display.h>
+
+const int bzpin = 15; //buzzer pin
+const int dpClk = 16; //display CLK
+const int dpDio = 17; //display DIO
+unsigned int cnt = 10; //timer count
+
+TM1637Display display(dpClk, dpDio);
+
+void setup()
+{
+  display.setBrightness(0x0a);
+  while (true) {
+    if (cnt == 0) {
+      display.showNumberDec(cnt);
+      tone(bzpin, 500, 3000);    
+      break;
+    }
+    display.showNumberDec(cnt);
+    cnt--;
+    delay(1000);
+  }
+}
+
+void loop()
+{
+
+}
